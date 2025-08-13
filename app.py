04aa849ae139a81643b273084f9189676d674966
AlanTN13 <fernandezalan46@gmail.com>
2025-08-13 13:27:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
+
 st.set_page_config(page_title="Smoke test", layout="wide")
 st.title("✅ La app arrancó")
+st.write("Si ves este mensaje, el deploy funciona. Ahora sí, subí tu app real.")
